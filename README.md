f45b083444142f1d7ab712e395a559fae36c31b8
Henry Lo <29010153+justaboredkid@users.noreply.github.com>
2018-11-29 20:33:50 -0800

    Adding asciigen.py

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Sacred
-ASCII art import. IN PYTHON. All you need to do is to grab some ascii art (from the web or somewhere else), covert it into json (Generator coming soon), and:
+ASCII art import. IN PYTHON. All you need to do is to grab some ascii art (from the web or somewhere else), convert it into json (using ./asciigen.py), and:
 ```
 import sacred
 
@@ -8,4 +8,4 @@ scene = sacred.Scene()
 scene.obj("test.json", 5, 10)
 ```
 
-*Documentation and ascii generator will be here soon*
+*Documentation will be here soon.*
---
+++ asciigen.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+'''
+
+Generates json file for render.py
+Note: NOT PERFECT. Adjust stuff yourself.
+
+{"line#":["text", SpaceBeforeText]}
+
+The actual process does not reqiure this script, you can 
+delete this if you want to.
+
+'''
+import json
+import re
+from sys import argv
+
+
+def write():
+    file = argv[2]
+    if file.rfind(".json") == -1:
+        file = argv[2] + ".json"
+
+    with open(file, "w") as s:
+        json.dump(obj, s)
+
+
+if len(argv) == 3:
+    infile = argv[1]
+    obj = {}
+    with open(infile, "r") as f:
+        ln = f.readlines()
+        splist = [re.search(r"[^ ]", l).start() for l in ln]
+        i = 1
+
+        for l in ln:
+            sp = min(s for s in splist)
+            if sp > 5:
+                obj[i] = [
+                    l[splist[i - 1]:].replace("\n", ""),
+                    round((splist[i - 1] - sp) / 5)
+                ]  # for better spacing in terminals
+            else:
+                obj[i] = [
+                    l[splist[i - 1]:].replace("\n", ""), (splist[i - 1] - sp)
+                ]
+            i += 1
+
+    write()
+
+else:
+    print("Usage: asciigen.py <inputfile> <outputname>")
