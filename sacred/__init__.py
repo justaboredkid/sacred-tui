94d48eac70c43293b8b5bc63c278a962f517db85
Henry Lo <29010153+justaboredkid@users.noreply.github.com>
2019-01-11 19:26:34 -0800

    Added camera, refactored rendering and clear, deprecated Scene.render()

--- sacred/__init__.py
+++ sacred/__init__.py
@@ -3,87 +3,99 @@ from time import sleep
 import os
 import sys
 import json
+import re
 # import threading
 
 t = Terminal()
-""" class mover(threading.Thread):
+screen = []
 
-    def start(self):
-        pass
 
-    def run(self, x1, y1, mx, my, leng, loop):
-
-        printobj(self.getName, x1, y1)
- """
-""" def main():
-    for x in range(4):
-        mythread = mover(name="Thread-{}".format(x + 1))
-        mythread.start() """
+def create_stage(w=t.width, h=t.height):
+    global screen
+    screen = [w * " " for i in range(0, h - 1)]
 
 
 class Scene(object):
 
-    def __init__(self, w=t.width, h=t.height, trpnt=True):
-        self.objs = []
-        self.pos = []
-        self.txts = []
-        self.width = w
-        self.height = h
+    def __init__(self):
+        if screen is None:
+            create_stage()
 
     def reset(self):
-        self.objs.clear()
-        self.pos.clear()
-        self.txts.clear()
+        global screen
+        screen = [len(screen[0]) * " " for i in range(0, len(screen))]
 
     def obj(self, file, x=0, y=0):  # loads ascii json files
-        if not os.path.isabs(file):
-            file = os.path.abspath(os.path.dirname(sys.argv[0])) + "/" + file
-        with open(file, "r") as o:
-            j = json.load(o)
-            self.objs.append(j)
+        global screen
+        try:
+            if not os.path.isabs(file):
+                file = os.path.abspath(os.path.dirname(
+                    sys.argv[0])) + "/" + file
+            with open(file, "r") as o:
+                j = json.load(o)
 
-            if "size" not in j.keys():  # 0.1.0 compatibility
-                s = []
-                for i in j.values():
-                    s.append(len(i[0]))
-                j["size"] = [max(s), len(j)]
+                for key, item in j.items():
+                    if key != "size":
+                        screen[y + int(key)] = screen[y + int(key)][:(
+                            x + item[1])] + item[0] + screen[y + int(key)][(
+                                x + (item[1] + len(item[0]))):]
 
-            self.pos.append([x, y])
-            o.seek(0)
-        return j
+                if "size" not in j.keys():  # 0.1.0 compatibility
+                    s = []
+                    for i in j.values():
+                        s.append(len(i[0]))
+                    j["size"] = [max(s), len(j)]
+            return j
+        except IndexError:
+            raise TooLarge
 
-    def txt(self, txt, x=0, y=0):
-        self.txts.append([txt, x, y])
+    def txt(self, txt: str, x=0, y=0):
+        global screen
+        try:
+            raw = re.sub(r"\x1b\[\d{1,3}m|\x1b\(B\x1b\[m", "", txt)  #ANSI stuff
+            screen[y] = screen[y][:x] + txt + screen[y][x + len(raw):]
+        except IndexError:
+            raise TooLarge
 
-    def box(self, x=0, y=0, w=t.width - 1, h=t.height - 1, fill=" "):
-        if len(fill) > 1:
-            raise ValueError  # allow only ONE char
-        box = {"1": ["-" * w, 0], str(h): ["-" * w, 0]}
-        if h == t.height - 1:
-            for i in range(1, int(h) - 1):
-                box[i] = ["|" + fill * (w - 2) + "|", 0]
-        else:
-            for i in range(2, int(h)):
-                box[i] = ["|" + fill * (w - 2) + "|", 0]
-        self.objs.append(box)
-        self.pos.append([x, y])
+    def box(self, x=0, y=0, w=t.width, h=t.height - 1, fill=" "):
+        global screen
+        try:
+            if len(fill) > 1:
+                raise ValueError  # allow only ONE char
 
-    def render(self):  # deprecate this?
-        i = 0
-        for obj in self.objs:
-            printobj(obj, self.pos[i][0], self.pos[i][1])
-            i += 1
+            screen[y] = screen[y][:x] + "-" * w + screen[y][x + w:]
+            screen[
+                y + h -
+                1] = screen[y + h - 1][:x] + "-" * w + screen[y + h - 1][x + w:]
 
-        for txt in self.txts:
-            with t.location(txt[1], txt[2]):
-                print(txt[0])
+            for i in range(0, int(h) - 2):
+                screen[y + i + 1] = screen[y + i][:x] + "|" + fill * (
+                    w - 2) + "|" + screen[y + i][x + w:]
+        except IndexError:
+            raise TooLarge
+
+    def render(self): #for unadjusted stages. deprecated.
+        global screen
+        frame = "\n".join(screen) + "\n"
+
+        sys.stdout.write(frame)
 
 
 class Camera(object):
+    global screen
 
-    def __init__(self, x, y):
+    def move(self, x, y):
         self.x = x
         self.y = y
+
+    def __init__(self, x=0, y=0):
+        self.move(x, y)
+
+    def render(self):
+        r = screen[self.y:self.y + t.height]
+        r = [r[i][self.x:self.x + t.width] for i in range(0, len(r))]
+
+        sys.stdout.write("\n".join(r) + "\n")
 
     # HOW DO YOU DO THISSSS?!?!?!??!?
 
@@ -95,7 +107,7 @@ class TooLarge(Exception):
 
         print(t.bold_red_on_bright_yellow("Scene exceeds terminal width"))
         print(
-            t.white_on_green(
+            t.bold_white_on_green(
                 "User: If possible, maximize the terminal window.\nDev: Make sure your x values are in check."
             ))
 
@@ -113,15 +125,6 @@ def fill(txt, delay=0, diag=True):  # fills screen with txt
             sleep(delay)
 
 
-def printobj(js, x, y):
-    for item, pos in js.items():
-        if item != "size":
-            if pos[1] + len(pos[0]) + x >= t.width:
-                raise TooLarge
-            with t.location(x, y + int(item)):
-                print(" " * pos[1] + pos[0], flush=True)
-
-
 def txtbox(txt, x=0, y=t.height - 1):
     with t.location(x, y):
         i = input(txt)
@@ -129,7 +132,7 @@ def txtbox(txt, x=0, y=t.height - 1):
 
 
 def clear():  # clears screen
-    print(t.clear())
+    print((t.width * " " + "\n") * t.height)
 
 
 if "__name__" == "__main__":
